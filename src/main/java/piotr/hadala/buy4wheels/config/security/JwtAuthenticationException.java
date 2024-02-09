c97c6ede9b7498730e672d200401a2d0827371a6
hadala-p <100712141+hadala-p@users.noreply.github.com>
2024-02-09 11:17:22 +0100

    Clean useless files

--- src/main/java/piotr/hadala/buy4wheels/config/security/JwtAuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package piotr.hadala.buy4wheels.config.security;
-
-import org.springframework.security.core.AuthenticationException;
-
-class JwtAuthenticationException extends AuthenticationException {
-    public JwtAuthenticationException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/piotr/hadala/buy4wheels/domain/user/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package piotr.hadala.buy4wheels.domain.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
-    Optional<UserRole> findByName(String name);
-}
