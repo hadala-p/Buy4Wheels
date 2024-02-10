33214bee66a35463536876a680dd0348526cee8a
hadala-p <phadala24@gmail.com>
2024-02-10 23:33:57 +0100

    Create auth service

---
+++ Buy4Wheels-auth/src/test/java/piotr/hadala/buy4wheelsauth/Buy4WheelsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package piotr.hadala.buy4wheelsauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buy4WheelsAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
