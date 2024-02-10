a5079f94fea149811748446f54b5dcd3e8fd4666
hadala-p <phadala24@gmail.com>
2024-02-10 23:35:09 +0100

    create user service

---
+++ Buy4Wheels-user/src/test/java/piotr/hadala/buy4wheelsuser/Buy4WheelsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package piotr.hadala.buy4wheelsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buy4WheelsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
