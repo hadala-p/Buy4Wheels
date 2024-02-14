d4ab67a07f247574d73d20e3e3ff7c74bde712ba
hadala-p <100712141+hadala-p@users.noreply.github.com>
2024-02-14 23:14:42 +0100

    Create car-service

---
+++ Buy4Wheels-car/src/main/java/piotr/hadala/buy4wheelscar/Buy4WheelsCarApplication.java
@@ -0,0 +1,13 @@
+package piotr.hadala.buy4wheelscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buy4WheelsCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Buy4WheelsCarApplication.class, args);
+    }
+
+}
---
+++ Buy4Wheels-car/src/test/java/piotr/hadala/buy4wheelscar/Buy4WheelsCarApplicationTests.java
@@ -0,0 +1,13 @@
+package piotr.hadala.buy4wheelscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buy4WheelsCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
