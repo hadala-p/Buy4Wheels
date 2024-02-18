cb996b2df6e1af7e941a89ba9c5a1bebd3ca5a9e
hadala-p <100712141+hadala-p@users.noreply.github.com>
2024-02-18 01:55:08 +0100

    Clean

--- user-service/src/main/java/piotr/hadala/userservice/config/security/JwtAuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package piotr.hadala.userservice.config.security;
-
-import org.springframework.security.core.AuthenticationException;
-
-class JwtAuthenticationException extends AuthenticationException {
-    public JwtAuthenticationException(String msg) {
-        super(msg);
-    }
-}
