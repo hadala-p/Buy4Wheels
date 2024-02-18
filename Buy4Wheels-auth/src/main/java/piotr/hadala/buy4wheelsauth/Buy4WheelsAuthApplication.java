d506f187650af46ef43b101ab93898b6175ca125
hadala-p <100712141+hadala-p@users.noreply.github.com>
2024-02-18 01:28:34 +0100

    Create auth-service

--- Buy4Wheels-auth/src/main/java/piotr/hadala/buy4wheelsauth/Buy4WheelsAuthApplication.java
+++ Buy4Wheels-auth/src/main/java/piotr/hadala/buy4wheelsauth/Buy4WheelsAuthApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Buy4WheelsAuthApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Buy4WheelsAuthApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Buy4WheelsAuthApplication.class, args);
+	}
 
 }
--- Buy4Wheels-auth/src/test/java/piotr/hadala/buy4wheelsauth/Buy4WheelsAuthApplicationTests.java
+++ Buy4Wheels-auth/src/test/java/piotr/hadala/buy4wheelsauth/Buy4WheelsAuthApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Buy4WheelsAuthApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
