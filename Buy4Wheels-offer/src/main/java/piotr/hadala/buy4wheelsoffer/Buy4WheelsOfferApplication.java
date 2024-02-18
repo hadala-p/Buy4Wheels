976c15e8d44fcb437e7a2549bca50e25ec0a703f
hadala-p <100712141+hadala-p@users.noreply.github.com>
2024-02-18 01:55:25 +0100

    Create offer-service

---
+++ Buy4Wheels-offer/src/main/java/piotr/hadala/buy4wheelsoffer/Buy4WheelsOfferApplication.java
@@ -0,0 +1,13 @@
+package piotr.hadala.buy4wheelsoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buy4WheelsOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buy4WheelsOfferApplication.class, args);
+	}
+
+}
---
+++ Buy4Wheels-offer/src/test/java/piotr/hadala/buy4wheelsoffer/Buy4WheelsOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package piotr.hadala.buy4wheelsoffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buy4WheelsOfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
